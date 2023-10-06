2f451e1f992c66501b3edff0525523f71e15ac99
SalahZirari1 <salahzirari@gmail.com>
2023-10-06 23:27:12 +0100

    default

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+
+/**
+* main - prints number if positive or negative or zero
+*
+*Return:0
+*/
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
