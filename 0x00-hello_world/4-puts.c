53b2473ad67c6e881ed2ddb3e3304bfe38ec4979
SalahZirari1 <salahzirari1@gmail.com>
2023-10-05 17:40:30 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+/**
+ * pritns a string
+ */
 
 int main(void)
 
 {
 	const char *msg = "\"Programming is like building a multilingual puzzle";
 	puts(msg);
+
 	return (0);
 }
