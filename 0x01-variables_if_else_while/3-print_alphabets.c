3530d70346d90e27d65a45fdf54a76c2d03f01d8
SalahZirari1 <salahzirari@gmail.com>
2023-10-06 22:29:31 +0100

    default

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - prints number if positive or negative or zero
+*
+*Return:0
+*/
+
+int main(void)
+{
+        int i;
+	int j;
+
+        for (i = 97; i < 123; i++)
+        {
+                putchar(i);
+        }
+
+        for (j = 65; j < 90; i++)
+        {
+                putchar(i);
+        }
+ 
+        putchar('\n');
+
+        return (0);
+}
