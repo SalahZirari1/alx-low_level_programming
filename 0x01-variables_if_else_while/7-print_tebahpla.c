c6cb3e614d64507eea37a461d48116e924d65408
SalahZirari1 <salahzirari@gmail.com>
2023-10-08 23:57:13 +0100

    default

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+* main - prints number if positive or negative or zero
+*
+*Return:0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
