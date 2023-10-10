72b85d84c7183ba144c2f94f4146345619a5d82d
SalahZirari1 <salahzirari@gmail.com>
2023-10-10 22:51:57 +0100

    default

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/*
+ * main - entry
+ *
+ * Return: 0
+ */
+
+int _putchar(char c);
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return 0;
+}
