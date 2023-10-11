5e7a8a1baf9b1f7bae13244db73c60d983f4233e
SalahZirari1 <salahzirari@gmail.com>
2023-10-12 00:31:26 +0100

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,15 +15,17 @@ int print_sign(int n)
 {
 	if ( n > 0 )
 	{
-		printf("+");
+		_putchar(43);
 		return (1);
 	}
 	if ( n < 0)
 	{
-		printf("-");
+		_putchar(45);
 		return (-1);
 	}
-
-	printf("0");
-	return (0);
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
