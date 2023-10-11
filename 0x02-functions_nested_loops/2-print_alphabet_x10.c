d24dfadc7efc715208a643b3b764a9bd0f4a963c
SalahZirari1 <salahzirari@gmail.com>
2023-10-11 23:39:37 +0100

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,14 +13,13 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j=0 ; j < 10; j++)
+	for (j = 0 ; j < 10; j++)
 	{
 		for (i = 97; i < 123; i++)
 		{
 			_putchar(i);
 		}
-		
+
 	_putchar('\n');
-	}
-	
+	}	
 }
