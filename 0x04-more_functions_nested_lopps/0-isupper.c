4e575d666a6e9904ef28204fb7d259497ba4feb9
SalahZirari1 <salahzirari@gmail.com>
2023-10-13 23:10:46 +0100

    commit

---
+++ 0x04-more_functions_nested_lopps/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - main func
+ * @c: int
+ * Return:
+ * -1: if upper
+ * -0:else
+ */
+
+
+int _isupper(int c)
+{
+	if ( c > 64 && c <91 )
+	{
+		return (1);
+	}
+
+	return (0);
+}
