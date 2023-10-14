e7a792cf8e265ad878823dbf78d070e657cbb2ee
SalahZirari1 <salahzirari@gmail.com>
2023-10-14 01:57:52 +0100

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,21 +9,23 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
+		if (i == 1)
+			print("%d", i);
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else
 		{
-			printf("%d ",i);
+			printf(" %d",i);
 		}
 	}
 	return (0);
