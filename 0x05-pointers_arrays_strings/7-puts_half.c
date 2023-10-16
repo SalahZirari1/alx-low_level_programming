670d3abe67b2f2829d639b1ea3ee9785e9e16efc
SalahZirari1 <salahzirari@gmail.com>
2023-10-17 00:18:23 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half 
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+	int start;
+
+	while (str[lenght] != '\0')
+	{
+		lenghth++
+	}
+	
+	if (lenght % 2 == 0)
+	{
+		start = length /2 ;
+	}
+	else
+	{
+		start = (length - 1) / 2;
+	}
+
+	while (str[start] != '\0')
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+}
