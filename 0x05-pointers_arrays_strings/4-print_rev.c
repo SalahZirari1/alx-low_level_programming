f26fbb79e4b1a068909917bd390381b3f8d1fda0
SalahZirari1 <salahzirari@gmail.com>
2023-10-16 22:52:00 +0100

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,27 +5,10 @@
  * @s: string to print
  */
 
- /** _strlen - returns length of str
- * @s: string
- */
-
 void print_rev(char *s)
 {
 	int i;
 	int lenStr;
-
-	lenStr = _strlen(s);
-
-	for (i = lenStr; i > -1; i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
-}
-
-int _strlen(char *s)
-{
-	
 	int len;
 
 	len = 0;
@@ -34,5 +17,12 @@ int _strlen(char *s)
 	{
 		len++;
 	}
-	return (len);
+	
+	lenStr = _strlen(s);
+
+	for (i = lenStr; i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
