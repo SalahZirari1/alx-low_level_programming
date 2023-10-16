64ec8d28d4a4a63586c2199e7453c432bd123248
SalahZirari1 <salahzirari@gmail.com>
2023-10-16 22:42:06 +0100

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
+ * print_rev - prints a string in reverse
  * @str: string to print
  */
 
-void _print_rev(char *s)
+void print_rev(char *s)
 {
         int i;
 	int lenStr;
