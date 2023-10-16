561fdb181fb2d78ad37156fb1d5af127c8cfbbaa
SalahZirari1 <salahzirari@gmail.com>
2023-10-17 00:39:34 +0100

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,11 +18,11 @@ void puts_half(char *str)
 	
 	if (length % 2 == 0)
 	{
-		start = length /2 ;
+		start = length / 2 ;
 	}
 	else
 	{
-		start = (length - 1) / 2;
+		start = (length + 1) / 2;
 	}
 
 	while (str[start] != '\0')
