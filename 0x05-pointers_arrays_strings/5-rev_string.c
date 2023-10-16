317d96fe00fe3578786a3f89ab6cffd8489d2f3b
SalahZirari1 <salahzirari@gmail.com>
2023-10-16 23:55:36 +0100

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,14 @@
  */
 
 
-void rev_string(char *s) 
+void rev_string(char *s)
 {
 	int length = strlen(s);
 	int i;
 	int j;
 	char temp;
 
-	for (i = 0, j = length - 1; i < j; i++, j--) 
+	for (i = 0, j = length - 1; i < j; i++, j--)
 	{
 		temp = s[i];
 		s[i] = s[j];
