a4ada81511beaec33d801066670b099ba097338a
SalahZirari1 <salahzirari@gmail.com>
2023-10-16 22:47:15 +0100

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,32 +2,34 @@
 
 /**
  * print_rev - prints a string in reverse
- * @str: string to print
+ * @s: string to print
+ *
+ * _strlen - returns length of str
+ * @s: string
  */
 
 void print_rev(char *s)
 {
-        int i;
+	int i;
 	int lenStr;
 
 	lenStr = _strlen(s);
-	
-        for (i = lenStr; i != 0; i--)
-        {
-                _putchar(s[i]);
-        }
-        _putchar('\n');
+
+	for (i = lenStr; i < 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
 
 int _strlen(char *s)
 {
-        int len;
+	int len;
+	len = 0;
 
-        len = 0;
-
-        while (s[len] != '\0')
-        {
-                len++;
-        }
-        return (len);
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
