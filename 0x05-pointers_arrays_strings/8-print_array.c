427fee1f37422e8aa65ecb50510488ad00076034
SalahZirari1 <salahzirari@gmail.com>
2023-10-17 23:20:40 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - pritns an array
+ * @a: array
+ * @n:number of elemets to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+
+	printf("%d",a[0]);	
+
+	for (i = 1; i < n; i++)
+	{
+		printf(", %d",a[i]);	
+	}
+}
