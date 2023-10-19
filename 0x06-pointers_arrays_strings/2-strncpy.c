4fae8ce3c9ee4827960e453d79f71e4f871cbb06
SalahZirari1 <salahzirari@gmail.com>
2023-10-19 23:04:29 +0100

    commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *dest_start = dest;
 
-	while (n > 0 && *src != '\0') 
+	while (n > 0 && *src != '\0')
 	{
 		*dest++ = *src++;
 		n--;
 	}
-	while (n > 0) 
+	while (n > 0)
 	{
 		*dest++ = '\0';
 		n--;
 	}
-	return dest_start;
+	return (dest_istart);
 }
