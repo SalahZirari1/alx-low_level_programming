16a78a35147b83eaac07b69bf644c22fdc1e5961
SalahZirari1 <salahzirari@gmail.com>
2023-10-19 23:29:58 +0100

    commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of ints
+ * @a:array
+ * @n:elements of array
+ *
+ */
+
+void reverse_array(int *a,int n)
+{
+	int start;
+	int end;
+
+	start = 0;
+	end = n - 1;
+
+	while (start < end)
+	{
+		int temp;
+
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+
+		start++;
+		end--;
+	}
+
+}
