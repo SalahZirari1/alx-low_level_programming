0bd4a888b6f683b89028d0f2b2c0998b654e2f76
SalahZirari1 <salahzirari@gmail.com>
2023-10-19 23:41:31 +0100

    commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * string_toupper - uppercases things
+ * @str:str
+ *
+ * Return: upper str
+ */
+
+char *string_toupper(char *str)
+{
+	char *ptr;
+
+	*ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - 32;
+		}
+		ptr++;
+	}
+
+	return (str);
+}
