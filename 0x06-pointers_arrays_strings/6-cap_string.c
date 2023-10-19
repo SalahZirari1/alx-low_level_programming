bfd93d8dd58e440360cfdea112237fd8abaa2cfe
SalahZirari1 <salahzirari@gmail.com>
2023-10-19 23:48:05 +0100

    commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_strign - capitalizes string
+ * @str: string
+ *
+ * Return: capitalized str
+ */
+
+char *cap_string(char *str)
+{
+	int capitalize;
+	char *ptr;
+
+	capitalize = 1;
+	ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' ||
+		*ptr == ',' || *ptr == ';' || *ptr == '.' || *ptr == '!' || *ptr == '?' ||
+		*ptr == '"' || *ptr == '(' || *ptr == ')' || *ptr == '{' || *ptr == '}') {
+		capitalize = 1;
+	}
+	else if (capitalize)
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr -= 32; 
+		}
+		capitalize = 0;
+		}
+		else if (*ptr >= 'A' && *ptr <= 'Z')
+		{
+			*ptr += 32; 
+		}
+		ptr++;
+	}
+	
+	return (str);
+}
