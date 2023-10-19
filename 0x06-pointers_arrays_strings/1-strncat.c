6172ec706437af5f33acfd1f76d565ba2e718b13
SalahZirari1 <salahzirari@gmail.com>
2023-10-19 22:46:37 +0100

    commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - concates two strings
+ * @dest: 1st str
+ * @src : 2nd str
+ * @n:n bytes
+ * 
+ * Return: pointer
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *og_dest = dest;  
+    
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	    
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	*dest = '\0';
+	return og_dest;
+}
