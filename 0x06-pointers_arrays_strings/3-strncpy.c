4f02c5eeae56eb88717cc536db9721223b7a70f1
SalahZirari1 <salahzirari@gmail.com>
2023-10-19 23:02:12 +0100

    commit

---
+++ 0x06-pointers_arrays_strings/3-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies string
+ *
+ * @dest: dsetination
+ * @src: source string
+ * @n: number of bytes
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *dest_start = dest;
+
+	while (n > 0 && *src != '\0') 
+	{
+		*dest++ = *src++;
+		n--;
+	}
+	while (n > 0) 
+	{
+		*dest++ = '\0';
+		n--;
+	}
+	return dest_start;
+}
