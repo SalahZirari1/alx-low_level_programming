e23a344484da08d589a02a260d7f875cb6ab98b9
SalahZirari1 <salahzirari@gmail.com>
2023-10-23 22:07:09 +0100

    msg

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,15 +5,18 @@
  * @s: memory set
  * @b: constant
  * @n: byte
+ *
+ * Return: pointer
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
 
-	return s;
+	return (s);
 }
