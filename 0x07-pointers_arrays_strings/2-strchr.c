a0fb895e51828d1e0195839d9b685f1b29cf8aad
SalahZirari1 <salahzirari@gmail.com>
2023-10-24 00:42:16 +0100

    msg

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a charachter in a sterling
+ * @s: string
+ * @c: charachter to locate
+ *
+ * Return: pointer or null 
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	while (s[i] != '/0')
+	{
+		if (s[i] == c)
+		{
+			return (*s[i]);
+		}
+	}
+
+	return (null);
+}
