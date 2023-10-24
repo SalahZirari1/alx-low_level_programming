608d16e255e235575ccc94888f64c636eec42901
SalahZirari1 <salahzirari@gmail.com>
2023-10-24 21:26:33 +0100

    msg

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - locates string
+ * @haystack: str to seach in
+ * @needle: substring to find
+ *
+ * Return: pointer or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *str, *k;
+
+	while (*haystack != '\0')
+	{
+		str = haystack;
+		k = needle;
+
+		while (*haystack == *k && *k != '\0')
+		{
+			haystack++;
+			k++;
+		}
+
+		if (*k == '\0')
+			return (str);
+		haystack = str + 1;
+	}
+	return (NULL);
+}
