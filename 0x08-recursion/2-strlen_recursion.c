4a102413eeeeaf96c0fa6731c92478a2be07a5da
SalahZirari1 <salahzirari@gmail.com>
2023-10-26 01:03:58 +0100

    msg

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - haha
+ * @s: str
+ *
+ * Return: len 
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+	
+	else 
+	{
+		return (0);
+	}
+
+	return (len);
+}
