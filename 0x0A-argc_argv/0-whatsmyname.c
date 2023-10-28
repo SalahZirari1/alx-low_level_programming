5db6fa8a93d470d2196d44cb6d36894d5d547abb
SalahZirari1 <salahzirari@gmail.com>
2023-10-29 00:45:46 +0100

    coumita

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,18 +2,22 @@
 
 /**
  * proggy - a program
+ * @argc:argc
+ * @argv:argv
  */
 
 void proggy(int argc, char* argv[])
 {
-	int i;
+	if (argc > 0)
+	{
+
 	char* name;
 
 	name = argv[0];
-
 	while ( *name != '\0')
 	{
 		_putchar(*name);
 		name++;
+	}
 	}
 }
