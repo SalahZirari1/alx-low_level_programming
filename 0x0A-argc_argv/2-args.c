3f39b8e867db340b128506a7b17deb51205a73ac
SalahZirari1 <salahzirari@gmail.com>
2023-10-29 18:14:04 +0100

    coumita

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -13,20 +13,12 @@ int main(int argc, char *argv[])
 {
 	int i;
 	
-	if ( argc > 1)
-	{
 		for (i = 0; i < argc; i++)
 
 		{
 
-			if (i != 1)
-				putchar('\n');
-	
 			printf("%s",argv[i]);
 
 		}
-		putchar('\n');
-	}
-
 	return (0);
 }
