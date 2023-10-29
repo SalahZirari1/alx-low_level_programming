61a1220828d153fc3d953b253bacbb24910fa1b8
SalahZirari1 <salahzirari@gmail.com>
2023-10-29 02:38:12 +0100

    coumita

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints things
+ * @argc: argc
+ * @argv:  argv
+ *
+ * Return: sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(argc);
+
+	return (0);
+}
