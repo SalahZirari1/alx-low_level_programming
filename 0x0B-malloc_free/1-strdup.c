e97f37caa627d3f0b7f44ca80853b4133a05d2f2
SalahZirari1 <salahzirari@gmail.com>
2023-10-30 23:50:08 +0100

    coumita

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,12 +11,16 @@
 char *_strdup(char *str)
 {
 	char *space;
-	int i;
+	int i, len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
 
 	if (str == NULL)
 		return (NULL);
 
-	space = (char *)malloc(sizeof(str));
+	space = (char *)malloc(len + 1);
 
 	i = 0;	
 	while (str[i] != '\0')
@@ -24,5 +28,7 @@ char *_strdup(char *str)
 		space[i] = str[i];
 		i++;
 	}
+
+	space[len] = '\0'
 	return (space);
 }
