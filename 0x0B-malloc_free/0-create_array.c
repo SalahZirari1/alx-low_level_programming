939e1a69f6334178bf605d5168a740aa568c9301
SalahZirari1 <salahzirari@gmail.com>
2023-10-30 23:21:34 +0100

    coumita

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,18 +11,20 @@
 
 char *create_array(unsigned int size, char c)
 {
+	
+	char *array;
+	unsigned int i;
+
 	if (size == 0)
 		return (NULL);
 
-	char *array;
-	int i;
 
 	array = (char *)malloc(size);
 
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i <size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
