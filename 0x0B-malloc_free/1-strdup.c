7218bb85eabe50809db561914d8cfdf7722e52b5
SalahZirari1 <salahzirari@gmail.com>
2023-10-30 23:38:10 +0100

    coumita

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - like strdup
+ * @str: string
+ *
+ * Return: null or pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *space;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	space = (char *)malloc(sizeof(str));
+
+	
+	while (str[i] != '\0')
+	{
+		space[i] = str[i];
+	}
+	return (space);
+}
