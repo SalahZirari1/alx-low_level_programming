1afae6d9b5b8a5f6300f778396619d5c1cd85f79
SalahZirari1 <salahzirari@gmail.com>
2023-10-31 20:45:07 +0100

    coumita

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - obvious
+ * @grid: a grid
+ * @height: the height
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+		return;
+
+	for (i = 0; i < height; i++)
+	free(grid[i]);
+
+	free(grid);
+}
