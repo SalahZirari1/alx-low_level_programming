1a59ec3ee963e8b4d6f2193000b8552444612904
SalahZirari1 <salahzirari@gmail.com>
2023-10-31 20:24:54 +0100

    coumita

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid -  some  craziness about a douple pointer god help me
+ * @width: width
+ * @height: height
+ *
+ * Return: NULL or pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, k, j;
+	int **grid;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+
+	grid = (int **)malloc(height * sizeof(int *)0);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			       free(grid[j]);
+
+			free(grid);
+			return (NULL);
+		}
+		for (k = 0; k < width; j++)
+			grid[i][k] = 0;
+	}
+
+	return (grid);
+}
