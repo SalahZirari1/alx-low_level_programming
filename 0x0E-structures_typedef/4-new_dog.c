1d9d60dfeda5f9d9bde61069d095e1547ab325db
SalahZirari1 <salahzirari@gmail.com>
2023-11-06 16:09:13 +0000

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,7 +23,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog->name = strdup(name);
 	new_dog->owner = strdup(owner);
-	new_dog->age = age;
 
-	return new_dog;
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+	}
+
+	new_dog->age = age;
+	return (new_dog);
 }
