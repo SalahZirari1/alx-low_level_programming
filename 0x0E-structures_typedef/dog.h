b5cfe1b11a552e9469e8f7c57790f4d520df52e7
SalahZirari1 <salahzirari@gmail.com>
2023-11-06 14:06:52 +0000

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - a dawg
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-	/* a dawg */
 };
 
 #endif
