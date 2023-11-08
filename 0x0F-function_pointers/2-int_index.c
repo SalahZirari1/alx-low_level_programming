e98a63e86805991347a1cc93e60388f6c64f50a1
SalahZirari1 <salahzirari@gmail.com>
2023-11-08 21:57:45 +0000

    commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - searches for an int
+ * @array: array
+ * @size: size
+ * @cmp: function pointer
+ *
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	
+	if(
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		       return (i);
+	}
+
+	return (-1)
+}
