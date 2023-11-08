a95ccb22e2f7c2999f1289d7c8e7b1e1641354e4
SalahZirari1 <salahzirari@gmail.com>
2023-11-08 22:02:23 +0000

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || cmp == NULL)
-		return -1;
+		return (-1);
 
 	if (size <= 0)
 		return (-1);
@@ -23,7 +23,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-		       return (i);
+			return (i);
 	}
 
 	return (-1);
