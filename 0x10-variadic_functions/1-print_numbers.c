325d4ecf2736e1bc2a87d2da326b509d7d0f55de
SalahZirari1 <salahzirari@gmail.com>
2023-11-09 20:22:32 +0000

    commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 
 	if (n == 0)
-		return (NULL);
+		return;
 	for(i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
