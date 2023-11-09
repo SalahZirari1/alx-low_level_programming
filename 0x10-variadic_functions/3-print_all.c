f94f83abf7544a8fb1725d4f6c286f96be5b5fb6
SalahZirari1 <salahzirari@gmail.com>
2023-11-09 22:04:49 +0000

    commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-#include <stdargs.h>
+#include <stdarg.h>
 
 /**
  * print_all - pritns anyting
@@ -22,22 +22,22 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(args, char));
+				printf("%c", va_arg(args, int));
 				break;
 			case 'i':
 				printf("%d", va_arg(args, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(args, float));
+				printf("%f", (float)va_arg(args, double));
 				break;
 			case 's':
-				s = va_args(args, char*);
+				s = va_arg(args, char*);
 				if ( s == NULL)
 					s = "(nil)";
 				printf("%s", s);
 				break;
 		}
-		if ((format[i + 1]) && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+		if ((format[i + 1]) && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
 				i++;
 	}
 
